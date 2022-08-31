543f9b5c1552841b84442dbac43bd00a23cac55c
= <=>
2022-08-31 08:07:23 +0530

    updated readme

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
-import recursion.Factorial;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(new Factorial().factorial(10));
+        System.out.println("Hello world");
     }
 }
